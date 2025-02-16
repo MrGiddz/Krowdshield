c76f3cfef232af3e41bc92f86a101cbd4bf0512b
mrgiddz <mideolaniyi@outlook.com>
2025-02-16 07:24:04 +0100

    question 5 completed

---
+++ question_5/format-date.js
@@ -0,0 +1,20 @@
+/**
+ *
+ * @param {string | number} date
+ *
+ * For this question, I will be adding a check to ensure that the correct data type is provided in the paramater and also ensure that the funtion returns a string value.
+ * 
+ * It will throw an error if the wrong data type is provided
+ * @throws string
+ * @returns string
+ */
+function formatDate(date) {
+  const isValidDate = Date.parse(date);
+  if (!isValidDate) throw new Error("Invalid date provided.");
+  let options = { year: "numeric", month: "2-digit", day: "2-digit" };
+  return new Date(date).toLocaleString("en-US", options);
+}
+console.log(formatDate("2024-04-10T14:30:00Z")); //Valid Date
+// console.log(formatDate("")); //Invalid Date
+console.log(formatDate({})); //Invalid Date
+console.log(formatDate("12")); //Invalid Date
